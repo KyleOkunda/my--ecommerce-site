f4b6655d3fdd73cb658ea7e03e87174037162a3f
Five <kylemuse08@gmail.com>
2025-02-02 13:40:10 +0300

    Added a functional shopping cart

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.listen(3001);
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -24,6 +25,43 @@ app.get("/", (req, res) => {
     res.render("index", { results, title: "All you can buy" });
   }
 
+  main();
+});
+
+app.get("/cart", (req, res) => {
+  async function main() {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      database: "ecommerce",
+      password: "KyleMuse@08",
+    });
+    const [results, field] = await connection.query("select * from cart");
+
+    res.render("cart", { results, title: "Shopping Cart" });
+  }
+  main();
+});
+
+app.post("/cart", (req, res) => {
+  let data = req.body;
+  console.log(data);
+  async function main() {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      database: "ecommerce",
+      password: "KyleMuse@08",
+    });
+
+    await connection.query("insert into cart() values(?, ?, ?, ?, ?)", [
+      data.prodid,
+      data.prodName,
+      data.imageLoc,
+      data.price,
+      data.quantity,
+    ]);
+  }
   main();
 });
 
